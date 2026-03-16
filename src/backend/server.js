4585f6a0e3ff70bc14655241162debac633e4d34
Nishith <vanganishith@gmail.com>
2026-03-16 20:15:58 +0530

    Added root API route

--- src/backend/server.js
+++ src/backend/server.js
@@ -18,6 +18,9 @@ app.use("/ai", aiRoutes);
 app.use("/notify", notifyRoutes);
 
 const PORT = process.env.PORT || 5000;
+app.get("/", (req, res) => {
+  res.send("CivicPulse API is running 🚀");
+});
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
